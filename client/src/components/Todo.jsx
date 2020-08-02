45ee81e438d5adda2841ab94849e8aa81d20e7d5
Jasper Chen <jasperdyb@gmail.com>
2020-08-02 13:17:22 +0800

    layout edit button

--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -1,5 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
+import { Button } from "react-bootstrap";
 
 export default function Todo({ name }) {
-  return <div className="todo text-wrap my-1 p-2 rounded">{name}</div>;
+  const [isOver, setIsOver] = useState(false);
+
+  function handleOnOver() {
+    setIsOver(true);
+  }
+
+  function handleOnLeave() {
+    setIsOver(false);
+  }
+
+  return (
+    <div
+      className="todo text-wrap my-1 p-2 rounded"
+      onMouseEnter={handleOnOver}
+      onMouseLeave={handleOnLeave}
+    >
+      {name}
+      {isOver && (
+        <Button className="edit-button m-1" size="sm">
+          <FontAwesomeIcon icon={faPencilAlt} />
+        </Button>
+      )}
+    </div>
+  );
 }
