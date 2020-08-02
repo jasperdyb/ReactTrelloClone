f7a8801917c8421fca78ae570496a490e7339f78
Jasper Chen <jasperdyb@gmail.com>
2020-08-02 15:33:44 +0800

    Build toggle editing function on NewList

--- client/src/components/NewList.jsx
+++ client/src/components/NewList.jsx
@@ -1,10 +1,42 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React, { useState, useRef, useEffect } from "react";
+import { Button, Form } from "react-bootstrap";
 
 export default function NewList() {
-  return (
-    <Button className="list p-2 m-1  rounded-lg new-list">
-      Add a new list...
-    </Button>
-  );
+  const [editing, updateEditing] = useState(false);
+  const editRef = useRef(null);
+
+  function toggleEditing() {
+    updateEditing(!editing);
+  }
+
+  useEffect(() => {
+    if (editing && editRef.current) {
+      editRef.current.focus();
+    }
+  }, [editing]);
+
+  if (!editing) {
+    return (
+      <Button
+        className="list p-2 m-1  rounded-lg new-list"
+        onClick={toggleEditing}
+      >
+        Add a new list...
+      </Button>
+    );
+  } else {
+    return (
+      <div className="list p-2 m-1  rounded-lg">
+        <Form>
+          <Form.Control
+            as="textarea"
+            rows="1"
+            placeholder="Name the new list.."
+            ref={editRef}
+            onBlur={toggleEditing}
+          />
+        </Form>
+      </div>
+    );
+  }
 }
