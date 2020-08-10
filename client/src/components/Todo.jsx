6ee8f653ca71afc4a9759ff3edee8f1b6f11d9ab
Jasper Chen <jasperdyb@gmail.com>
2020-08-10 14:44:10 +0800

    setup drag source

--- client/src/components/Todo.jsx
+++ client/src/components/Todo.jsx
@@ -2,6 +2,8 @@ import React, { useState, useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 import { Button } from "react-bootstrap";
+import { useDrag } from "react-dnd";
+import { ItemTypes } from "../dnd/constants.js";
 
 export default function Todo({ name, listId, todoId, updateEditState }) {
   const [isOver, setIsOver] = useState(false);
@@ -18,7 +20,7 @@ export default function Todo({ name, listId, todoId, updateEditState }) {
   function handelClickEdit(e) {
     e.preventDefault();
 
-    const { top, left, width } = targetRef.current.getBoundingClientRect();
+    const { top, left, width } = drag.current.getBoundingClientRect();
     updateEditState({
       show: true,
       dimensions: {
@@ -32,19 +34,32 @@ export default function Todo({ name, listId, todoId, updateEditState }) {
     });
   }
 
+  const [{ isDragging }, drag] = useDrag({
+    item: { listId, todoId, type: ItemTypes.TODO },
+    collect: (monitor) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  });
+
   return (
-    <div
-      className="todo text-wrap my-1 p-2 rounded"
-      ref={targetRef}
-      onMouseEnter={handleOnOver}
-      onMouseLeave={handleOnLeave}
-    >
-      {name}
-      {isOver && (
-        <Button className="edit-button m-1" size="sm" onClick={handelClickEdit}>
-          <FontAwesomeIcon icon={faPencilAlt} />
-        </Button>
-      )}
+    <div ref={drag}>
+      <div
+        className="todo text-wrap my-1 p-2 rounded"
+        ref={targetRef}
+        onMouseEnter={handleOnOver}
+        onMouseLeave={handleOnLeave}
+      >
+        {name}
+        {isOver && (
+          <Button
+            className="edit-button m-1"
+            size="sm"
+            onClick={handelClickEdit}
+          >
+            <FontAwesomeIcon icon={faPencilAlt} />
+          </Button>
+        )}
+      </div>
     </div>
   );
 }
