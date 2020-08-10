7820697907f83fadecbe0bc16bd1d9a66506f865
Jasper Chen <jasperdyb@gmail.com>
2020-08-10 15:17:17 +0800

    Build list wrapper for drop

--- client/src/components/List.jsx
+++ client/src/components/List.jsx
@@ -19,39 +19,41 @@ export default function List({
   }
 
   return (
-    <div className="list p-2 m-1 rounded-lg">
-      <ListTitle
-        title={title}
-        editList={editList}
-        listId={listId}
-        updateMenuState={updateMenuState}
-      />
-      {todos.map((todo, index) => (
-        <Todo
-          key={index}
-          {...todo}
+    <div className="list-wrapper">
+      <div className="list p-2 m-1 rounded-lg">
+        <ListTitle
+          title={title}
+          editList={editList}
           listId={listId}
-          todoId={index}
-          // updateEditState={updateEditState}
+          updateMenuState={updateMenuState}
         />
-      ))}
-      {showNew && (
-        <NewTodo
-          listId={listId}
-          toggleShowNew={toggleShowNew}
-          addTodo={addTodo}
-        />
-      )}
-      {!showNew && (
-        <div className="footer d-flex">
-          <Button
-            className="py-1 flex-grow-1 text-left"
-            onClick={toggleShowNew}
-          >
-            + New
-          </Button>
-        </div>
-      )}
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            {...todo}
+            listId={listId}
+            todoId={index}
+            // updateEditState={updateEditState}
+          />
+        ))}
+        {showNew && (
+          <NewTodo
+            listId={listId}
+            toggleShowNew={toggleShowNew}
+            addTodo={addTodo}
+          />
+        )}
+        {!showNew && (
+          <div className="footer d-flex">
+            <Button
+              className="py-1 flex-grow-1 text-left"
+              onClick={toggleShowNew}
+            >
+              + New
+            </Button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
