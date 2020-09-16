6209e4dfcc5d5cb57d61a89b94e8075d4a5f86aa
Jasper Chen <jasperdyb@gmail.com>
2020-09-16 17:12:04 +0800

    Add error message

--- client/src/components/KanBanMenu.jsx
+++ client/src/components/KanBanMenu.jsx
@@ -3,21 +3,32 @@ import WallpaperChoice from "../containers/WallpaperChoice";
 
 export default function KanBanMenu({ kanBanMenuState, updateMenuState }) {
   const [unsplashUrls, setUrls] = useState([]);
+  const [connectionError, setError] = useState(false);
 
   useEffect(() => {
     fetch(
       `https://api.unsplash.com/photos/random/?client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}&count=20&orientation=landscape&query=nature`
     )
       .then((res) => {
-        return res.json();
+        if (res.status === 200) {
+          return res.json();
+        }
+        throw Object.assign(new Error("connection error"), {
+          code: res.status,
+        });
       })
-      .then((jsonData) => {
-        const flitteredData = jsonData.map((picData) => ({
-          id: picData.id,
-          url: `${picData.urls.raw}&fm=jpg&w=800&h=450&fit=max`,
-        }));
-        setUrls(flitteredData);
-      });
+      .then(
+        (jsonData) => {
+          const flitteredData = jsonData.map((picData) => ({
+            id: picData.id,
+            url: `${picData.urls.raw}&fm=jpg&w=800&h=450&fit=max`,
+          }));
+          setUrls(flitteredData);
+        },
+        (err) => {
+          setError(true);
+        }
+      );
   }, []);
 
   function handleClick(e) {
@@ -48,6 +59,12 @@ export default function KanBanMenu({ kanBanMenuState, updateMenuState }) {
       <div className="menu-title w100 text-center py-2">Pick a wallpaper</div>
       <hr className="my-0 mx-3" />
       <div className="wallpaper-collection py-1">
+        {connectionError && (
+          <div className="error-message">
+            <p>Error while trying to connect Unsplash.</p>
+            <p>Please try again later.</p>
+          </div>
+        )}
         {unsplashUrls.map((wallpaper) => {
           return (
             <WallpaperChoice
